3cd3062392b3b0d075588205f406cf895347fc9c
Johan August Østbye <johan.august@outlook.com>
2022-01-24 12:44:13 +0100

    added a auth check for page creation

--- pages/tacoday/new.tsx
+++ pages/tacoday/new.tsx
@@ -1,5 +1,20 @@
+import { useSession } from 'next-auth/react';
+import { Router, useRouter } from 'next/router';
 import React from 'react';
 import Layout from '../../components/layout';
+import FullLoader from '../../components/sections/FullLoader';
 export default function New() {
+  const router = useRouter();
+  const session = useSession();
+  if (session.status == 'loading') {
+    return (
+      <Layout>
+        <FullLoader />
+      </Layout>
+    );
+  }
+  if (session.status == 'unauthenticated') {
+    router.push('api/auth/signin');
+  }
   return <Layout>new</Layout>;
 }
