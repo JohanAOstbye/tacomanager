943c2b6dd88881fae305fa476055d6435e0c6743
Johan August Østbye <johan.august@outlook.com>
2022-02-25 17:31:35 +0100

    font error

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -11,11 +11,11 @@ type Props = {
 const Layout = ({ children, className }: Props) => {
   return (
     <div
-      className={`${className} text-gray-700 flex flex-col items-center justify-center min-h-screen max-w`}
+      className={`${className} text-gray-700 flex flex-col items-center justify-center min-h-screen max-w font-montserrat `}
     >
       <Head title="Taco" />
       <Nav />
-      <main className="font-montserrat flex flex-col items-center justify-center flex-1 px-5 m-5 my-10">
+      <main className="flex flex-col items-center justify-center flex-1 px-5 m-5 my-10">
         {children}
       </main>
       <Footer />
